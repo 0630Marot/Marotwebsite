6fff868cbd73b93504d8b85ce30f4a0d26bc4c90
0630Marot <2185720896@qq.com>
2023-07-16 23:49:39 +0800

    new file:   index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((request, response) => {
+    let html = fs.readFileSync(__dirname + '/index.html')
+    response.end(html)
+}
+)
+
+server.listen(9000, () => {
+    console.log('sevrer on !')
+}
+)
