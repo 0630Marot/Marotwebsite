e60a6b63f4dc6ce8f93f7bedf41f87dee8250630
0630Marot <2185720896@qq.com>
2023-07-18 02:37:43 +0800

    modified:   routes/index.js 	modified:   routes/users.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
+const express = require('express');
+const router = express.Router();
+router.use(express.static('./public'));
+/* GET home. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('respond with a resource');
 });
-
 module.exports = router;
